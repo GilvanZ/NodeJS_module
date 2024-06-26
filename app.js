a17cba0951b9f46e9999d3b48f715258aea980ab
Gill <gilvan.zacarias@outlook.com>
2024-06-26 09:18:59 -0300

    done

---
+++ app.js
@@ -0,0 +1,7 @@
+
+// Import the greet function from the module.js file
+const greet = require('./module');
+
+// Use the greet function and print the result
+const greetingMessage = greet('Gilvan Zack');
+console.log(greetingMessage);
---
+++ module.js
@@ -0,0 +1,7 @@
+// Function to greet a user by name
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+
+// Export the function to be used in other files
+module.exports = greet;
